23df4cb86a0980d4b7a04427d133e23de1d85e7c
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 12:35:17 +0900

    Hello, 대한민국으로 변경 Hello, Yamji로 출력을 추가

--- Ch01_HelloWorld/src/ch01/hello/Hello.java
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("Hello, Java");
-		System.out.println("Hello, Korea");
+		System.out.println("Hello, 대한민국");
+		System.out.println("Hello, Yamji");
 	}
 }
