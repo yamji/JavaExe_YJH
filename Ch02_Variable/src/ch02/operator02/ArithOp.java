d8f4e2e7cddcce83ea7783bc7893e7217838253d
ymaji <yanjihee19@naver.com>
2024-01-24 16:32:41 +0900

    ArithOp.Java 사칙연산 코드 추가

---
+++ Ch02_Variable/src/ch02/operator02/ArithOp.java
@@ -0,0 +1,13 @@
+package ch02.operator02;
+
+public class ArithOp {
+	public static void main(String[] args) {
+		int num0 = 10;
+		int num1 = 3;
+		System.out.println(num0+num1);
+		System.out.println(num0-num1);
+		System.out.println(num0*num1);
+		System.out.println(num0/num1); //몫
+		System.out.println(num0%num1); //나머지
+	}
+}
