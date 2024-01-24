f9c9c7c62e8a99121b4e0d370668575fe39d9e22
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 11:34:14 +0900

    Hello, Korea 출력 추가

--- Ch01_HelloWorld/src/ch01/hello/Hello.java
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("Hello, Java");
+		System.out.println("Hello, Korea");
 	}
 }
