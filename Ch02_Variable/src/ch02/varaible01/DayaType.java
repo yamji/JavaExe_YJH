5f8f5ac7818aca9cbfa8dfeb50b0ce74080949e6
ymaji <bitcamp1@bitcamp>
2024-01-24 15:43:10 +0900

    name1 추가 출력

---
+++ Ch02_Variable/src/ch02/varaible01/DayaType.java
@@ -0,0 +1,23 @@
+package ch02.varaible01;
+
+public class DayaType {
+	public static void main(String[] args) {
+		// 자료형 변수공간 이름 = 값 
+		int num = 100;			//정수
+		double dnum = 3.14;		//실수
+		char ch = 'a';			//문자 1글자
+		String name = "홍길동";	//문자열
+		boolean isRun = true;	//true/false
+		String str = "a";		//문자열
+		String name1 = "admin";	//문자열
+		
+		System.out.println(num);
+		num = 999;
+		System.out.println(num);
+		System.out.println(dnum);
+		System.out.println(ch);
+		System.out.println(name);
+		System.out.println(isRun);
+		System.out.println(str);
+	}
+}
