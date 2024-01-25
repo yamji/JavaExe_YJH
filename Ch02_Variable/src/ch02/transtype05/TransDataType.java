37f68fe9a3a97e421c14c1ff9081d605b16e1beb
ymaji <yanjihee19@naver.com>
2024-01-25 11:02:48 +0900

    타입변환 추가

---
+++ Ch02_Variable/src/ch02/transtype05/TransDataType.java
@@ -0,0 +1,26 @@
+package ch02.transtype05;
+
+public class TransDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int total = 297;  //총점
+		int stNum = 4;   //학생수
+		double avg = 0.; //평균
+		System.out.println("총점은 " + total + "입니다. ");
+		System.out.println("평군은 " + (total/stNum) + "입니다. ");
+		avg = total/stNum;
+		System.out.println("평균은 " + avg + "입니다. ");
+		// (double)total : 컴파일러에 의해 정수 total은 double로 변환
+		/* 실수와 정수 연산의 결과는?
+		 *실수의 표현범위 > 정수
+		 *결과값은 실수 자료형으로 나온다.
+		 */
+		avg = (double)total/stNum;
+		System.out.println("평균은 " + avg + "입니다. ");
+		
+		int nAng = (int)avg;
+		System.out.println("평균은 " + nAng + "입니다.");
+	}
+
+}
