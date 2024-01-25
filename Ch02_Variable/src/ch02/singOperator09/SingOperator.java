6e96945ef958e5ccd236b7cf8c2e7be4b2ed37f9
ymaji <yanjihee19@naver.com>
2024-01-25 11:41:33 +0900

    sing 연산 추가

---
+++ Ch02_Variable/src/ch02/singOperator09/SingOperator.java
@@ -0,0 +1,16 @@
+package ch02.singOperator09;
+
+public class SingOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = +100;
+		int num1 = -num;
+		int num2 = -num1;
+		System.out.println(num);
+		System.out.println(num1);
+		System.out.println(num2);
+
+	}
+
+}
