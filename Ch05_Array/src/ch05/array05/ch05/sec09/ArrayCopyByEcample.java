3b04fe51c931a0e04f764562b2bc689001d2b7b2
ymaji <yanjihee19@naver.com>
2024-01-26 17:08:59 +0900

    187~190 예제문제 실습

---
+++ Ch05_Array/src/ch05/array05/ch05/sec09/ArrayCopyByEcample.java
@@ -0,0 +1,19 @@
+package ch05.array05.ch05.sec09;
+
+public class ArrayCopyByEcample {
+
+	public static void main(String[] args) {
+		int[] oldIntArray = {1,2,3};
+		int[] newIntArray = new int [5];
+		
+		for(int i=0; i<oldIntArray.length; i++) {
+			newIntArray[i] = oldIntArray[i];
+		}
+		
+		for(int i=0; i<newIntArray.length; i++) {
+			System.out.print(newIntArray[i] + ", ");
+		}
+
+	}
+
+}
