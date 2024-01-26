87b6c7a3fa0448d7d4c73a0e358021587dbd927a
ymaji <yanjihee19@naver.com>
2024-01-26 11:44:05 +0900

    for10 Resolve2.java 추가

--- Ch03_Control/src/ch03/for10/Resolve2.java
+++ Ch03_Control/src/ch03/for10/Resolve2.java
@@ -2,26 +2,41 @@ package ch03.for10;
 
 import java.util.Scanner;
 
+//2. 369게임을 작성합니다.
+//1~99까지의 정수를 입력받고
+//3,6,9중 하나가 있으면 "박수짝"을 출력하고
+//2개가 있으면 "박수짝짝"을 출력하세요
+//예를 들어 13은 "박수짝"
+//36인 경우는 "박수짝짝"을 출력하면 됩니다.
 public class Resolve2 {
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	
-	System.out.println("1~99까지 정수 입력(음수면 종료) >>");
+	while(true) {
+		System.out.println("1~99까지 정수 입력(음수면 종료) >>");
 		int num = sc.nextInt();
 		if(num < 0)
 			break;
-	
+		
 		int n10 = num/10;
 		int n1 = num%10;
 	
 		int cnt = 0;
-		if(n10==3 || n10==5 || n10==9)
+		if(n10==3 || n10==6 || n10==9)
 			cnt++;
-	
-		sc.close();
-	
-		sc.close();
-			}
+		if(n10==3 || n10==6 || n10==9)
+			cnt++;
 		
+		if(cnt==1)
+			System.out.println("박수짝");
+		else if(cnt==2)
+			System.out.println("박수짝짝");
 	}
+	System.out.println("End~");
+	
+	sc.close();
+	
+	}
+}
+		
 
