89adedc8f479b2fb394cc339d3a1c7e38c409d66
ymaji <yanjihee19@naver.com>
2024-01-29 18:06:39 +0900

    1/29 최종 업로드

--- Ch05_Array/src/ch05/array04/Reslorve6.java
+++ Ch05_Array/src/ch05/array04/Reslorve6.java
@@ -1,5 +1,22 @@
 package ch05.array04;
 
+import java.util.Scanner;
+
 public class Reslorve6 {
+	public static void main(String[] args) {
+		String[] nameArr = new String[5];
+		int[] scoreArr = new int[5];
+		char[] chScoreArr = new char[5];
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=0;i<nameArr.length; i++) {
+			System.out.print((i+1) + "번째 학생 이름 >> ");
+			String name = sc.nextLine();
+			System.out.print((i+1) + "번째 학생 점수 >> ");
+			String strScore = sc.nextLine();
+			int score =
+		}
+	}
 
 }
---
+++ Ch07_Eunm/src/ch07/enum01/ch05/sec12/Week.java
@@ -0,0 +1,12 @@
+package ch07.enum01.ch05.sec12;
+
+public enum Week {
+	MONDAY,
+	TUESDAY,
+	WENESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+
+}
