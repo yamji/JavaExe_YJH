45a8fe24491b11e4e04500617a258439e9c41840
ymaji <yanjihee19@naver.com>
2024-01-29 12:20:00 +0900

    정수 배열 최소값 최대값 문제풀이

--- Ch05_Array/src/ch05/array08/Resolve.java
+++ Ch05_Array/src/ch05/array08/Resolve.java
@@ -2,6 +2,18 @@ package ch05.array08;
 //1. 정수 배열 5개를 할당하고
 //Arrays.sort를 활용해서 최소값과 최대값을 얻으세요
 
+import java.util.Arrays;
+
 public class Resolve {
+	public static void main(String[] args) {
+		int[] numArr = {10, 99, 2, 78, 32};
+		
+		System.out.println(Arrays.toString(numArr));
+		Arrays.sort(numArr);
+		System.out.println(Arrays.toString(numArr));
+		
+		System.out.println("최소값은 " + numArr[0]);
+		System.out.println("최대값은 " + numArr[numArr.length-1]);
+	}
 
 }
--- Ch05_Array/src/ch05/array08/Resolve2.java
+++ Ch05_Array/src/ch05/array08/Resolve2.java
@@ -4,5 +4,23 @@ package ch05.array08;
 //Arrays.sort를 사용하지 말고 최소값과 최대값을 얻으세요
 
 public class Resolve2 {
+	public static void main(String[] args) {
+		int[] numArr = {10, 99, 2, 78, 32, 1, 67, 105, 187, 23};
+		
+		int min, max;
+		min = max = numArr[0];
+		
+		for(int i=1; i<numArr.length; i++) {
+			
+			if(min> numArr[i])
+				min = numArr[i];
+			
+			if(max < numArr[i])
+				max = numArr[i];
+		}
+		
+		System.out.println("최소값은 " + min);
+		System.out.println("최대값은 " + max);
+	}
 
 }
