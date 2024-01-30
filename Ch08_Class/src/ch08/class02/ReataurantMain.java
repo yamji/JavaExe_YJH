780b7ab3abfbe4171033e2773c92d179c6b2dc40
ymaji <yanjihee19@naver.com>
2024-01-30 14:34:47 +0900

    ch08.class02

---
+++ Ch08_Class/src/ch08/class02/ReataurantMain.java
@@ -0,0 +1,12 @@
+package ch08.class02;
+
+public class ReataurantMain {
+	public static void main(String[] args) {
+		Restaurant rest = new Restaurant();
+		rest.viewMenu();
+		rest.selectFoodMenu();
+		rest.deliveryFoodMenu();
+		rest.keyboardEnd();
+	}
+
+}
---
+++ Ch08_Class/src/ch08/class02/Restaurant.java
@@ -0,0 +1,51 @@
+package ch08.class02;
+
+import java.util.Scanner;
+
+/*
+ * 클래스는 영역이 확실하나(클래스의 캡슐화)
+ * 클래스 = 멤버변수(필드) + 멤버함수(메서드)
+ * 
+ * 식당
+ * 필드=변수=명사
+ * 메서드=함수=동사
+ * 
+ * [클래스의 대상]
+ * 큰 개념의 명사(일반적인 클래스)
+ * 동사들의 집합(controller, Business Layer)
+ * 필드들의 집합(Vo, Dao)
+ * 
+ * */
+public class Restaurant {
+	// 필드
+	private String[] foods = {"냉면", "떡볶이", "쌀국수", 
+			"칼국수", "순대", "튀김", "라면"};
+	private int selectNum;
+	private Scanner sc = new Scanner(System.in);
+	
+	//메서드
+	public void viewMenu() {
+		for(int i =0; i<foods.length; i++) {
+			System.out.println(i + "." + foods[i]);
+		}
+	}
+	
+	public void selectFoodMenu() {
+		System.out.print("어떤 음식을 선택하시겠습니까? >> ");
+		selectNum = sc.nextInt();
+		
+	}
+
+	public void deliveryFoodMenu() {
+		String food = foods[selectNum];
+		System.out.println(food + "이 나왔습니다.");
+		System.out.println("맛있게 드세요 ^^");
+	
+	}
+	public void keyboardEnd() {
+		sc.close();
+		
+	}
+
+	}
+
